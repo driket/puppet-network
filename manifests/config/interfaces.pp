a3bd10549d6c5623b1a64b1e456fd03408e95cb7
Cedric Villa <cedric.villa@gmail.com>
2013-07-01 14:58:29 +0200

    Code clean

--- manifests/config/interfaces.pp
+++ manifests/config/interfaces.pp
@@ -4,26 +4,25 @@ define mv_network::config::interfaces ($fqdn = 'sd-21553.dedibox.fr',
 								$network = '88.191.142.0', 
 								$broadcast = '88.191.142.255', 
 								$gateway = '88.191.142.1',
-								$template_file = 'network_interfaces.erb',) {
+								$template_file = 'network_interfaces.erb',) 
+{
 	require('mv_network')
 	require('mv_network::packages')
-
-    file { '/etc/network/interfaces': 
-        ensure => file,
-        content => template("mv_network/${template_file}"),
-        mode => "0644",
-        owner => 'root',
-        group => 'root',
-#		notify => Service['networking'],
-#		notify => [Service['networking'],Exec['ifup_br0']],
-    }
 	
+	file { '/etc/network/interfaces': 
+	
+		ensure => file,
+		content => template("mv_network/${template_file}"),
+		mode => "0644",
+		owner => 'root',
+		group => 'root',
+		#notify => Service['networking'],
+		#notify => [Service['networking'],Exec['ifup_br0']],
+    }
+		
 	service {'networking':
-#		ensure => 'running',
+		#ensure => 'running',
 		#refreshonly => true,
 	} 
-	
-    #exec { "ifup_br0":
-    #     command => "/sbin/ifup br0",
-    #}
+
 }
